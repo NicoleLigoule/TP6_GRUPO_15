3d17a68608d0a2a38b64f3460171708721c73bf1
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-10-07 15:43:40 -0300

    PersonaDao
    Creacion de interfaz personaDao

--- src/dao/PersonaDao.java
+++ src/dao/PersonaDao.java
@@ -1,5 +1,13 @@
 package dao;
 
-public interface PersonaDao {
+import java.util.List;
 
+import entidad.Persona;
+
+public interface PersonaDao {
+	
+	public boolean insert(Persona persona);
+	public boolean modificar(Persona persona_modificar);
+	public boolean delete(Persona persona_a_eliminar);
+	public List<Persona> readAll();
 }
