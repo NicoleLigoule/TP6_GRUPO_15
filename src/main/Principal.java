4b69bd08121a8e0de4f8f6c8f4a3f773a3595aab
ferrerotute <matiasferrerov@gmail.com>
2024-10-09 01:15:45 -0300

    Package prensentacion.vista
    Se crea el package presentacion.vista
    Se mueve la logica de dibujado de la ventana de Principal a la vista de la presentacion

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,16 +1,9 @@
 package main;
 
-
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+
+import presentacion.vista.VentanaPrincipal;
 
 public class Principal extends JFrame {
 
@@ -18,47 +11,8 @@ public class Principal extends JFrame {
 
     public static void main(String[] args) {
         // Inicia la ventana
-        Principal frame = new Principal();
-        frame.setVisible(true); // Hace visible la ventana
+        VentanaPrincipal vista = new VentanaPrincipal();
+        vista.show();
     }
 
-    public Principal() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
-
-        contentPane = new JPanel();
-        contentPane.setLayout(new BorderLayout(0, 0));
-        setContentPane(contentPane);
-
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
-
-        JMenu mnPersona = new JMenu("Persona");
-        mnPersona.setHorizontalAlignment(SwingConstants.LEFT);
-        mnPersona.setBackground(Color.WHITE);
-        mnPersona.setFont(new Font("Segoe UI", Font.PLAIN, 18));
-        mnPersona.setForeground(Color.BLACK);
-        menuBar.add(mnPersona);
-
-        // Opción Agregar
-        JMenuItem MnIAgregar = new JMenuItem("Agregar");
-        MnIAgregar.setHorizontalAlignment(SwingConstants.LEFT);
-        mnPersona.add(MnIAgregar);
-
-        // Opción Modificar
-        // comment de prueba
-        JMenuItem MnIModificar = new JMenuItem("Modificar");
-        MnIModificar.setHorizontalAlignment(SwingConstants.LEFT);
-        mnPersona.add(MnIModificar);
-
-        // Opción Eliminar
-        JMenuItem MnIEliminar = new JMenuItem("Eliminar");
-        MnIEliminar.setHorizontalAlignment(SwingConstants.LEFT);
-        mnPersona.add(MnIEliminar);
-
-        // Opción Listar
-        JMenuItem MnIListar = new JMenuItem("Listar");
-        MnIListar.setHorizontalAlignment(SwingConstants.LEFT);
-        mnPersona.add(MnIListar);
-    }
 }
