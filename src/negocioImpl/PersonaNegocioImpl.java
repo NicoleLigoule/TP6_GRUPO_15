64eb89e53bef958fdd9a6ac06bb9876b752fccf9
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-10-09 09:00:46 -0300

    Paquete Negocio y NegocioImpl

---
+++ src/negocioImpl/PersonaNegocioImpl.java
@@ -0,0 +1,34 @@
+package negocioImpl;
+
+import java.util.List;
+
+import entidad.Persona;
+import negocio.PersonaNegocio;
+
+public class PersonaNegocioImpl implements PersonaNegocio{
+
+	@Override
+	public boolean insert(Persona persona) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificar(Persona persona_modificar) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Persona persona_a_eliminar) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Persona> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
